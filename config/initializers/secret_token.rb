88d9425356ec2b7043ea9cb344c6767602aa7290
Justin Lambert <mastfish@gmail.com>
2012-10-26 18:06:44 +1100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HouseDo::Application.config.secret_token = '5d642726c762c42e53e10932898714e71712a3e770bd68fac58f2f8ed7cd1741a3a1a5a3a81f73dade6f33e5dd7e8db2e36f1af727d12500e17c765238c7d059'
