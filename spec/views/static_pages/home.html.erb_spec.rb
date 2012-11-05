6650062f3cbee7e3f694a8eff5dbd471511fb384
Justin Lambert <mastfish@gmail.com>
2012-11-05 15:49:26 +1100

    removed erronous view test

--- spec/views/static_pages/home.html.erb_spec.rb
+++
@@ -1,8 +0,0 @@
-  describe "home page" do
-    before {visit  root_path}
-
-
-    it { should have_selector('h1',    text: 'House_do') }
-    it { should have_selector('title', text: 'Sign up') }
-    it { should have_content('Welcome') }
-  end
