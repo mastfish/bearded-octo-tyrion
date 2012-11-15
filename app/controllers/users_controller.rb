517da1e02a711222611fe5d6c4d0304af7473b3f
Justin Lambert <mastfish@gmail.com>
2012-11-15 12:48:39 +1100

    user editing is now properly restricted

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,23 +1,25 @@
 class UsersController < ApplicationController
+  before_filter :signed_in_user, only: [:edit, :update]
+  before_filter :correct_user,   only: [:edit, :update]
 
   def show
-    @user = User.find(params[:id])
+  @user = User.find(params[:id])
   end
 
-def new
+  def new
     @user = User.new
-end
+  end
 
-def create
-    @user = User.new(params[:user])
-    if @user.save
-      sign_in @user
-      flash[:success] = "Welcome to sharehous.in!"
-      redirect_to @user
-    else
-      render 'new'
-    end
-end
+  def create
+      @user = User.new(params[:user])
+      if @user.save
+        sign_in @user
+        flash[:success] = "Welcome to sharehous.in!"
+        redirect_to @user
+      else
+        render 'new'
+      end
+  end
 
   def edit
     @user = User.find(params[:id])
@@ -33,4 +35,17 @@ end
       render 'edit'
     end
   end
+
+  private
+    def signed_in_user
+      unless signed_in?
+        store_location
+        redirect_to signin_url, notice: "Please sign in."
+      end
+    end
+
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_path) unless current_user?(@user)
+    end
 end
